9a028687af7f65c5a0d052906336a9fd689d09f3
charlie-riesmeyer <C.riesmeyer7@outlook.com>
2025-08-11 23:48:49 -0400

    Real initial commit

---
+++ src/main/java/com/riesmeyer/schedulenotifier/ScheduleNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.riesmeyer.schedulenotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleNotifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleNotifierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/riesmeyer/schedulenotifier/ScheduleNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riesmeyer.schedulenotifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleNotifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
